ce6a406bfb89ababa2a40501723a590a5bc2acb4
Shravan R Tandel <shravansrt10@gmail.com>
2021-02-23 20:43:09 +0530

    Completed

--- dbmsP/settings.py
+++ dbmsP/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'dbmsP.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bluebusDB',
+        'USER': 'root',
+        'PASSWORD': 'hellohi1007',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
